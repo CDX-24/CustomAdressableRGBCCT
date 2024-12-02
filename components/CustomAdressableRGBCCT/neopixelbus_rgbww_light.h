2a3bf9b5034578485e2594ee31bd45823d7ec674
Chris D. <49126303+CDX-24@users.noreply.github.com>
2024-12-03 00:29:42 +0100

    Update neopixelbus_rgbww_light.h

--- components/CustomAdressableRGBCCT/neopixelbus_rgbww_light.h
+++ components/CustomAdressableRGBCCT/neopixelbus_rgbww_light.h
@@ -17,15 +17,12 @@ namespace lsc_rgbww {
 
 class LSCRGBWWLightOutput : public light::LightOutput {
  public:
-  // void set_red(uint8_t *red) { red_ = red; }
-  // void set_green(uint8_t *green) { green_ = green; }
-  // void set_blue(uint8_t *blue) { blue_ = blue; }
-  // void set_cold_white(uint8_t *cold_white) { cold_white_ = cold_white; }
-  // void set_warm_white(uint8_t *warm_white) { warm_white_ = warm_white; }
   void config(uint16_t count_pixels, uint8_t pin) {
-    this->strip = new NeoPixelBus<NeoGrbcwxFeature, NeoWs2812xMethod>(count_pixels, pin);
-    this->strip->Begin();  // Example initialization method
-    this->strip->Show();
+    this->strip_ = new NeoPixelBus<NeoGrbcwxFeature, NeoWs2812xMethod>(count_pixels, pin);
+  }
+  void setup() override {
+    this->strip_->Begin();  // Example initialization method
+    this->strip_->Show();
   }
   void set_cold_white_temperature(float cold_white_temperature) { cold_white_temperature_ = cold_white_temperature; }
   void set_warm_white_temperature(float warm_white_temperature) { warm_white_temperature_ = warm_white_temperature; }
@@ -46,17 +43,12 @@ class LSCRGBWWLightOutput : public light::LightOutput {
     float red, green, blue, cwhite, wwhite;
     state->current_values_as_rgbww(&red, &green, &blue, &cwhite, &wwhite, this->constant_brightness_);
     RgbwwColor color = RgbwwColor(red, green, blue, cwhite, wwhite);
-    this->strip->SetPixelColor(0, color);
-    this->strip->Show();
+    this->strip_->SetPixelColor(0, color);
+    this->strip_->Show();
   }
 
  protected:
-  NeoPixelBus<NeoGrbcwxFeature, NeoWs2812xMethod> *strip;
-  uint8_t *red_;
-  uint8_t *green_;
-  uint8_t *blue_;
-  uint8_t *cold_white_;
-  uint8_t *warm_white_;
+  NeoPixelBus<NeoGrbcwxFeature, NeoWs2812xMethod> *strip_;
   float cold_white_temperature_{0};
   float warm_white_temperature_{0};
   bool constant_brightness_;
